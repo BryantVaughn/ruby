9768a8bf320260c0479ddd4bf5894f7aa0760c5f
BryantVaughn <bryant.f.vaughn@gmail.com>
2020-08-24 13:21:37 -0600

    converting types

---
+++ nums_and_bools/convert_nums_to_strs.rb
@@ -0,0 +1,15 @@
+num = 5
+p num
+p num.class 
+
+# string class
+p num.to_s.class
+
+# float class
+p num.to_f.class
+
+pi = 3.14
+
+p pi.class # float
+p pi.to_i.class # int
+p pi.to_s.class # string
