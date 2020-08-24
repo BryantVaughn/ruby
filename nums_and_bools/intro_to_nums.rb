db1517b64dc56f8a20a83e0a4256472aa805015b
BryantVaughn <bryant.f.vaughn@gmail.com>
2020-08-24 12:29:45 -0600

    number classes

---
+++ nums_and_bools/intro_to_nums.rb
@@ -0,0 +1,13 @@
+# ints
+p 5.class
+p 100.class
+p 0
+
+#floats
+p 5.987.class
+p 2.71.class
+p -10.23
+
+# Bignum
+# deprecated in ruby 2.4
+p 999999999999999999999999999999999999999999999999.class
